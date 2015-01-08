3199a6e732f5b66ea10f64ea7518e51386ea54b6
lizhe <lizhe19851001@126.com>
2015-01-08 20:15:55 +0800

    m

--- web/web.go
+++ web/web.go
@@ -1,25 +1,67 @@
 package web
 
 import (
-	// "fmt"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 )
 
 var (
-	templates = template.Must(template.ParseFiles("web/template/application.html"))
+	templatesDir = "web/template/"
+	templatesMap map[string]*template.Template
+	templates    = template.Must(template.ParseGlob("web/template/*.html"))
 )
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", nil)
+func getPathName(path string) string {
+	base := filepath.Base(path)
+	ext := filepath.Ext(path)
+	name := base[:strings.LastIndex(base, ext)]
+	return name
+}
+
+func init() {
+	if templatesMap == nil {
+		templatesMap = make(map[string]*template.Template)
+	}
+
+	layouts, err := filepath.Glob(templatesDir + "layouts/*.html")
+	if err != nil {
+		log.Panic(err)
+	}
+	views, err := filepath.Glob(templatesDir + "views/*.html")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, vl := range layouts {
+		for _, vv := range views {
+			lName := getPathName(vl)
+			vName := getPathName(vv)
+			key := fmt.Sprintf("%s_%s", lName, vName)
+			t := template.Must(template.New("").ParseFiles(vl, vv))
+			templatesMap[key] = t
+		}
+	}
+}
+
+func render(w http.ResponseWriter, tmplName string) {
+	lName := "base"
+	key := fmt.Sprintf("%s_%s", lName, tmplName)
+	t, ok := templatesMap[key]
+	if !ok {
+		http.Error(w, "No such template.", http.StatusInternalServerError)
+	}
+	err := t.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "application")
+	render(w, "root")
 }
 
 func Start() {
