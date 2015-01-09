8b726d8fb80b2f60f7c5924e38c9054bc056fb7b
lizhe <lizhe19851001@126.com>
2015-01-09 21:12:25 +0800

    m

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,1 +1,7 @@
-var hello = "hello"
+$(document).ready(function () {	
+	ckb_toggle_all = $('#ck_box_toggle_all');
+	ckb_toggle_all.click(function () {
+            var $this = $(this);
+            $('.bulk_edit_ck_box').prop('checked', $this.is(':checked'));
+	});
+});
